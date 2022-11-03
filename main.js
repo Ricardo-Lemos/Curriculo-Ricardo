a20a6559d50f82fc7d5fb0c589e2150066d50994
Ricardo Lemos <102613365+Ricardo-Lemos@users.noreply.github.com>
2022-11-03 16:03:58 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var nome = prompt("Qual seu nome?")
+if (nome = "nome") {
+    alert("Bem Vindo");
+};
+
+
+
+
+
+
+var nome = "Ricardo Lemos de Souza";
+var idade = " 26"
+var curriculo = "Curriculo"
+alert(curriculo + " de " + nome);
